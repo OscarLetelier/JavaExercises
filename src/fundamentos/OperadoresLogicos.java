7c9f4c34504c5648bb1be9fe90a55bd1c49b7762
Oscar Letelier Molina <oscar.letelier@outlook.com>
2025-04-16 16:47:55 -0400

    Ejercicio 5 - Operador Logico

--- src/fundamentos/OperadoresLogicos.java
+++ src/fundamentos/OperadoresLogicos.java
@@ -2,15 +2,24 @@ package fundamentos;
 
 import java.util.Scanner;
 
+// Deteminar si la persona puede ingresar, o si debe ingresar junto a un adulto
+
 public class OperadoresLogicos {
     public static void main(String[] args) {
+
+        int edadMinima = 14;
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Ingresa el Primer Numero a Comparar");
-        int num1 = input.nextInt();
+        System.out.println("Sistema para Ingresar a Pelicula");
+        System.out.println("Ingresa su Edad : ");
+        int edad = input.nextInt();
 
-        System.out.println("Ingrese el Segundo Numero a Comparar");
-        int num2 = input.nextInt();
-
-        // Operadores Logicos
+        if (edad < edadMinima){
+            System.out.println("No puede Ingresar");
+        } else if (edad == edadMinima && edad <= edadMinima) {
+            System.out.println("Ud. puede Ingresar Junto a un Adulto");
+        } else {
+            System.out.println("Ud. puede ingresar");
+        }
     }
 }
