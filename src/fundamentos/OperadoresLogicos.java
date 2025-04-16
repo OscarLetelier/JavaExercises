e1ddee5974ebdd21be9de4b5c22c26f2b16f37fe
Oscar Letelier Molina <oscar.letelier@outlook.com>
2025-04-16 16:34:08 -0400

    Ejercicio 4 - Operadores de Comparacion

--- src/fundamentos/OperadoresLogicos.java
+++ src/fundamentos/OperadoresLogicos.java
@@ -1,4 +1,16 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class OperadoresLogicos {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingresa el Primer Numero a Comparar");
+        int num1 = input.nextInt();
+
+        System.out.println("Ingrese el Segundo Numero a Comparar");
+        int num2 = input.nextInt();
+
+        // Operadores Logicos
+    }
 }
