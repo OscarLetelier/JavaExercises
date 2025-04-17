fe4d5a816ccdd0ed734936c405c8c8956d9bc1d9
Oscar Letelier Molina <oscar.letelier@outlook.com>
2025-04-17 00:45:04 -0400

    Conversor de Celcius a Fahrenheit y de Fahrenheit a Celcius

---
+++ src/fundamentos/CelciusAFahrenheit.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class CelciusAFahrenheit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese Grados Celcius");
+        double celcius = input.nextDouble();
+
+        double resultado = ((celcius * 9.0 / 5.0) + 32);
+        System.out.println("Los grados Fahrenheit son : "+resultado);
+    }
+}
