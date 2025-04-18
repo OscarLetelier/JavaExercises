7fe8ee63b49201e2d55911ac257cdcb320005368
Oscar Letelier Molina <oscar.letelier@outlook.com>
2025-04-18 16:50:50 -0400

    Bucle For Iteración del 1 al 10

---
+++ src/fundamentos/BucleFor.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+/*
+Objetivo:
+Imprimir numeros del 1 al 10
+ */
+
+public class BucleFor {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al Iterador de Ciclo For");
+
+        int i;
+
+        for(i = 1; i <=10; i++){
+            System.out.println("Iteracion de " +i);
+        }
+    }
+}
