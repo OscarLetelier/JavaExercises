7bca267291133ff46c540f7de4e706fd597a4a5f
Oscar Letelier Molina <oscar.letelier@outlook.com>
2025-04-24 00:11:25 -0400

    Ejercicio do while junto a carpeta con clases separadas y menu interactivo

---
+++ src/fundamentos/clasesdowhile/FahrenheitACelcius.java
@@ -0,0 +1,16 @@
+package fundamentos.clasesdowhile;
+
+import java.util.Scanner;
+
+public class FahrenheitACelcius {
+    public static void ConversorDeFahretheitACelcius(){
+        Scanner input = new Scanner(System.in);
+
+        final int CONVERSOR = 32;
+        System.out.println("Ingrese Grados Fahrentheit");
+        double fahrentheit = input.nextDouble();
+
+        double resultado = (fahrentheit - CONVERSOR) * 5/9;
+        System.out.println("El grado Fahrentheit a Celcius es : "+resultado);
+    }
+}
